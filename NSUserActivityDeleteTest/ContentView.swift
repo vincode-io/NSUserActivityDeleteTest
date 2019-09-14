669a6d6b13ab19d06644b8d8f326a0a06fb86165
Maurice Parker <mo@vincode.io>
2019-09-14 14:09:17 -0500

    Added test case

--- NSUserActivityDeleteTest/ContentView.swift
+++ NSUserActivityDeleteTest/ContentView.swift
@@ -9,9 +9,39 @@
 import SwiftUI
 
 struct ContentView: View {
+	
+	@State var activity: NSUserActivity? = nil
+	
     var body: some View {
-        Text("Hello World")
+		VStack {
+			Spacer()
+			Button(action: { self.createActivity() }) {
+				Text("Create Activity")
+			}
+			Divider().frame(width: 100.0, height: 1.0, alignment: .center)
+			Button(action: { self.deleteActivity() }) {
+				Text("Delete Activity")
+			}
+			Spacer()
+		}
     }
+
+	func createActivity() {
+		activity = NSUserActivity(activityType: "io.vincode.NSUserActivityDeleteTest.TestActivity")
+		activity!.title = "NSUserActivity Delete Test"
+		activity!.keywords = Set(["NSUserActivity"])
+		activity!.isEligibleForPrediction = true
+		activity!.isEligibleForSearch = true
+		activity!.persistentIdentifier = "the.identifier.to.delete"
+		activity!.becomeCurrent()
+	}
+	
+	func deleteActivity() {
+		activity?.invalidate()
+		activity = nil
+		NSUserActivity.deleteSavedUserActivities(withPersistentIdentifiers: ["the.identifier.to.delete"]) {}
+	}
+	
 }
 
 struct ContentView_Previews: PreviewProvider {
---
+++ README.md
@@ -0,0 +1,14 @@
+#  NSUserActivityDelete
+
+This project is intended to show that donated Siri Shortcuts aren't being deleted when requested to do so
+using the NSUserActivity API.
+
+Step 1: Build and Launch NSUserActivityDeleteTest application on iOS 13 or greater
+Step 2: Press the Create Activity button
+Step 3: Return to the Home Screen.  Pull down to enter Search.
+Step 4: Verify that the Shortcut was donated by searching for "NSUserActivity" 
+Step 5: Verify that you see a Shortcut for the application "NSUSERACTIVITYDELETETEST"
+Step 6: Return to the NSUserActivityDeleteTest application
+Step 7: Press the Delete Activity button
+Step 8: Return to the Home Screen and perform the same steps to find the donataed Shortcut
+Step 9: Invalid Result:  The donated shortcut still appears
