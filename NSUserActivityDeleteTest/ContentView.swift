fd5b48d4595f494865be24d24b44883cf01fa151
Maurice Parker <mo@vincode.io>
2019-09-14 13:44:39 -0500

    Initial Commit

---
+++ NSUserActivityDeleteTest/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  NSUserActivityDeleteTest
+//
+//  Created by Maurice Parker on 9/14/19.
+//  Copyright © 2019 Vincode, Inc. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
